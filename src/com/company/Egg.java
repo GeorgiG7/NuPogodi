ff9aad3469c4f6c33e2b025583d989a812560797
Gosho <gogogospodinov69@gmail.com>
2020-05-30 06:16:59 +0300

    Logic almost done

--- src/com/company/Egg.java
+++ src/com/company/Egg.java
@@ -4,13 +4,14 @@ public class Egg {
 
     private int posX;
     private int posY;
+    private int chickenNumber;
     private String path;
     private Textures texture;
 
-    public Egg(int posX, int posY, int width, int height, String path) {
-        this.posX = posX;
-        this.posY = posY;
-        this.texture = new Textures(posX, posY, width, height, path);
+    public Egg(int chickenNumber) {
+        this.chickenNumber = chickenNumber;
+        setCoordinatesByNumberOfChicken(chickenNumber);
+        this.texture = new Textures(this.posX, this.posY, 25, 25, "Resources\\egg1.png");
     }
 
     public void setPosY(int posY) {
@@ -21,13 +22,38 @@ public class Egg {
         return posY;
     }
 
+    public int getChickenNumber() {
+        return chickenNumber;
+    }
+
     public Textures getTexture() {
         updateTexture();
         return texture;
     }
 
-    public void updateTexture(){
+    public void updateTexture() {
         this.texture.setX(this.posX);
         this.texture.setY(this.posY);
+    }
+
+    private void setCoordinatesByNumberOfChicken(int numberOfChicken) {
+        switch (numberOfChicken) {
+            case 1:
+                this.posY = 25;
+                this.posX = 46;
+                break;
+            case 2:
+                this.posX = 113;
+                this.posY = 25;
+                break;
+            case 3:
+                this.posX = 177;
+                this.posY = 25;
+                break;
+            case 4:
+                this.posX = 242;
+                this.posY = 25;
+                break;
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,51 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Display.initInstance(600, 600, "Nancy");
 
+        JLayeredPane background = new JLayeredPane();
+        background.setPreferredSize(new Dimension(600, 600));
+
+        Egg[] eggs = new Egg[100];
+
         //Textures egg = new Textures(75, 30, 20, 20, "Resources\\egg.png");
-        Textures nuPogodi = new Textures(20, 200, 180, 180, "Resources\\NuPogodi.png");
-        Textures chickenFirst = new Textures(30, -1,100, 80, "Resources\\chickenAnimated.png");
-        Textures chickenSecond = new Textures(95, -1,100, 80, "Resources\\chickenAnimated.png");
-        Textures chickenThird = new Textures(160, -1,100, 80, "Resources\\chickenAnimated.png");
-        Textures chickenForth = new Textures(225, -1,100, 80, "Resources\\chickenAnimated.png");
-        Textures grass = new Textures(0, 220,600, 150, "Resources\\grass.png");
-        Textures wood = new Textures(0, 30,600, 150, "Resources\\wood.png");
+        //Textures nuPogodi = new Textures(20, 190, 200, 200, "Resources\\NuPogodi.png");
+        Textures chickenFirst = new Textures(30, -1, 100, 80, "Resources\\chickenAnimated.png");
+        Textures chickenSecond = new Textures(95, -1, 100, 80, "Resources\\chickenAnimated.png");
+        Textures chickenThird = new Textures(160, -1, 100, 80, "Resources\\chickenAnimated.png");
+        Textures chickenForth = new Textures(225, -1, 100, 80, "Resources\\chickenAnimated.png");
+        Textures grass = new Textures(0, 220, 600, 150, "Resources\\grass.png");
+        Textures wood = new Textures(0, 30, 600, 150, "Resources\\wood.png");
 
-        Egg egg = new Egg(75,30,25, 25,"Resources\\egg1.png");
+        Egg egg = new Egg(4);
+        eggs[0] = egg;
+        NuPogodi nuPogodi1 = new NuPogodi(4);
 
-        Display.getInstance().addGraphic(grass);
+//        Display.getInstance().addGraphic(grass);
+//        Display.getInstance().addGraphic(wood);
+//        Display.getInstance().addGraphic(egg.getTexture());
+//        Display.getInstance().addGraphic(nuPogodi1.getTexture());
+//        Display.getInstance().addGraphic(chickenFirst);
+//        Display.getInstance().addGraphic(chickenForth);
+//        Display.getInstance().addGraphic(chickenSecond);
+//        Display.getInstance().addGraphic(chickenThird);
+//        Display.getInstance().addGraphic(wood);
+//        Display.getInstance().addGraphic(grass);
+
+        int y = 5;
+
+        int iterationsCounter = 0;
+
         Display.getInstance().addGraphic(wood);
         Display.getInstance().addGraphic(egg.getTexture());
-        Display.getInstance().addGraphic(nuPogodi);
+        Display.getInstance().addGraphic(nuPogodi1.getTexture());
         Display.getInstance().addGraphic(chickenFirst);
         Display.getInstance().addGraphic(chickenForth);
         Display.getInstance().addGraphic(chickenSecond);
@@ -28,23 +53,84 @@ public class Main {
         Display.getInstance().addGraphic(wood);
         Display.getInstance().addGraphic(grass);
 
-        int y = 1;
+        int score = 0;
 
         while (true) {
+            iterationsCounter++;
+
             try {
-                Thread.sleep(10);
+                Thread.sleep(100);
             } catch (InterruptedException ex) {
                 Thread.currentThread().interrupt();
             }
 
-            egg.setPosY(egg.getPosY() + y);
-
-            egg.getTexture().render();
-            if (egg.getPosY() < 0 || egg.getPosY() > 280) {
-                y *= -1;
+            for (int i = 0; i < eggs.length; i++) {
+                if (eggs[i] != null){
+                    eggs[i].setPosY(eggs[i].getPosY() + y);
+                    eggs[i].getTexture().render();
+                    if (eggs[i].getPosY() == 225 && eggs[i].getChickenNumber() == nuPogodi1.getPosition()) {
+                        score++;
+                        Display.getInstance().removeGraphic(getComponentIndex(eggs[i].getTexture()));
+                        eggs[i] = null;
+                    } else if (eggs[i].getPosY() > 280){
+                        Display.getInstance().removeGraphic(getComponentIndex(eggs[i].getTexture()));
+                        eggs[i] = null;
+                    }
+                }
             }
 
-            System.out.println(egg.getPosY());
+            //egg.setPosY(egg.getPosY() + y);
+
+            //egg.getTexture().render();
+//            if (egg.getPosY() < 25 || egg.getPosY() > 225) {
+//                y *= -1;
+//            }
+
+            if (iterationsCounter == 20) {
+                eggs = addEggsByRandomPosition(eggs);
+                iterationsCounter = 0;
+                nuPogodi1.setPosition(ThreadLocalRandom.current().nextInt(1, 5));
+                nuPogodi1.getTexture().render();
+            }
+//            System.out.print(egg.getPosY());
+//            System.out.println("   " + getLastNotEmptyIndexOfArray(eggs) + "   " + score);
+
         }
+
+    }
+
+    public static int getFirstEmptyIndexOfArray(Egg[] eggs) {
+
+        int openArray = 0;
+
+        for (int i = 0; i < eggs.length; i++) {
+            if (eggs[i] == null) {
+                openArray = i;
+                break;
+            }
+        }
+        return openArray;
+    }
+
+    public static Egg[] addEggsByRandomPosition(Egg[] eggs) {
+        int firstEmptyIndex = getFirstEmptyIndexOfArray(eggs) + 1;
+
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 5);
+        eggs[firstEmptyIndex] = new Egg(randomNum);
+        Display.getInstance().addGraphic(eggs[firstEmptyIndex].getTexture());
+
+        return eggs;
+    }
+
+    public static int getComponentIndex(Component component) {
+        if (component != null && component.getParent() != null) {
+            Container c = component.getParent();
+            for (int i = 0; i < c.getComponentCount(); i++) {
+                if (c.getComponent(i) == component)
+                    return i;
+            }
+        }
+
+        return -1;
     }
 }
