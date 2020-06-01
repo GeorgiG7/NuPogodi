9cd8d0b3ac9825cd385687e7879180a0441070e5
GoSh069 <60853386+GoSh069@users.noreply.github.com>
2020-06-01 15:53:50 +0300

    final: NuPogodi

--- src/com/company/NuPogodi.java
+++ src/com/company/NuPogodi.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class NuPogodi {
 
     private int posX;
@@ -9,6 +12,7 @@ public class NuPogodi {
     private Textures texture;
 
     public NuPogodi(int position) {
+        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(keyEventDispatcher);
         this.position = position;
         setCoordinatesAndTextureByNumberOfChicken(position);
         this.texture = new Textures(this.posX, this.posY, 200, 200, this.texturePath);
@@ -22,11 +26,14 @@ public class NuPogodi {
         return position;
     }
 
-    public void setPosition(int position) {
-        this.position = position;
+    public void updateTexture() {
+        this.texture.setX(this.posX);
+        this.texture.setY(this.posY);
+        this.texture.setPath(this.texturePath);
+        this.texture.render();
     }
 
-    private void setCoordinatesAndTextureByNumberOfChicken(int numberOfChicken) {
+    public void setCoordinatesAndTextureByNumberOfChicken(int numberOfChicken) {
         switch (numberOfChicken) {
             case 1:
                 this.posY = 190;
@@ -50,4 +57,23 @@ public class NuPogodi {
                 break;
         }
     }
+
+    KeyEventDispatcher keyEventDispatcher = new KeyEventDispatcher() {
+        @Override
+        public boolean dispatchKeyEvent(KeyEvent e) {
+            if (e.getID() == KeyEvent.KEY_PRESSED) {
+                if (e.getKeyCode() == KeyEvent.VK_A) {
+                    position -= position==1?0: 1;
+                    setCoordinatesAndTextureByNumberOfChicken(position);
+                    updateTexture();
+                }
+                if (e.getKeyCode() == KeyEvent.VK_D) {
+                    position += position==4?0: 1;
+                    setCoordinatesAndTextureByNumberOfChicken(position);
+                    updateTexture();
+                }
+            }
+            return false;
+        }
+    };
 }
